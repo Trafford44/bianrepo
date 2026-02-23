1c339f30bdb7b72b2f2f762f201376797d72aa63
Trafford44 <nzrs@outlook.com>
2026-02-23 22:17:12 +1300

    Implement gist_id setting via URL
    Add functionality to set gist_id from URL parameters.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// TEMP: allow setting gist_id via URL
+(function () {
+    const params = new URLSearchParams(window.location.search);
+    const gist = params.get("set_gist");
+    if (gist) {
+        localStorage.setItem("gist_id", gist);
+        console.log("Gist ID set via URL:", gist);
+    }
+})();
+
 import { handleOAuthRedirect, bindLoginButton } from "./auth.js";
 import { initResizers, renderSidebar, bindEditorEvents,  bindPaneFocusEvents, updateLoginIndicator } from "./ui.js";
 import { setSubjects } from "./ui.js";
