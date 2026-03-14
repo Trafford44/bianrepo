820d0eaaf35c6519e6ef5920514fd888a90dcfa6
Trafford44 <nzrs@outlook.com>
2026-03-14 16:11:31 +1300

    Update device.js

--- device.js
+++ device.js
@@ -5,14 +5,15 @@ import { logger } from "./logger.js";
 logger.debug("device", "device.js loaded from:", import.meta.url);
 
 let deviceId = localStorage.getItem("deviceId");
-logger.debug("device", "Loaded deviceId from localStorage:", deviceId);
+logger.debug("device", "Loaded deviceId from localStorage:", String(deviceId));
 
 if (!deviceId) {
     deviceId = crypto.randomUUID();
     localStorage.setItem("deviceId", deviceId);
-    logger.debug("device", "Generated NEW deviceId:", deviceId);
+    logger.debug("device", "Generated NEW deviceId:", String(deviceId));
 } else {
-    logger.debug("device", "Using EXISTING deviceId:", deviceId);
+    logger.debug("device", "Using EXISTING deviceId:", String(deviceId));
 }
 
 export { deviceId };
+
