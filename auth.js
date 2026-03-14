0ff30bbdb4a87603fa9413d0a727f6c2152e9534
Trafford44 <nzrs@outlook.com>
2026-03-14 15:07:50 +1300

    Update auth.js

--- auth.js
+++ auth.js
@@ -40,6 +40,8 @@ import { runSyncCheck } from "./sync.js";
 import { deviceId } from "./device.js";
 import { logger } from "./logger.js";
 
+logger.debug("auth","auth.js loaded from:", import.meta.url);
+
 const GITHUB_CLIENT_ID = "Ov23likIpQOhuNITyTEh";
 const WORKER_URL = "https://round-rain-473a.richard-191.workers.dev";
 
